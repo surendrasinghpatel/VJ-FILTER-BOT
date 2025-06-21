088becc355ea2e7262f1a50da159542ae33c950a
surendrasinghpatel <meeradevi22103@gmail.com>
2025-06-21 15:42:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg')).split()
+PICS = (environ.get('PICS', 'https://i.ibb.co/ch3n9wsf/x.jpg')).split()
 
 
 # Admins & Users
@@ -91,7 +91,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+sAIZx9peDdMyZjU9')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+tPP6b2Qwl9w4MTQ1')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'moviesandwebseriesji') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/+tPP6b2Qwl9w4MTQ1')
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/mrperfectjikabot')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
